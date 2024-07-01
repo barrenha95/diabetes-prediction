bc00329b05cf5ace68b0df93c75aa03ccfd9fcdf
isabarrenha <barrenha95@yahoo.com.br>
2024-07-01 11:24:09 -0300

    start-class-exploratory-analysis

--- scripts/utilities.py
+++ scripts/utilities.py
@@ -1,1 +1,19 @@
 # General-purpose utility scripts or helper functions.
+
+import pandas as pd
+
+class ExploratoryAnalysis:
+    """This class was created to turn exploratory analysis easier"""
+
+    def __init__(self, dataframe, target):
+        self.dataframe = dataframe
+        self.target = target
+
+
+"""Debug generating an generic small dataframe to check if the functions works properly"""
+if __name__ == '__main__':
+    data = [['tom', 'athletic','f',18, 0],['erika', 'sedentary','f',22, 0],['robert', 'sedentary','m',56, 1]]
+    df = pd.DataFrame(data, columns=['Name', 'Activity', 'Sex', 'Age', 'Diabetes'])
+    analysis = ExploratoryAnalysis(dataframe = df, target = 'Diabetes')
+    print(analysis.dataframe)
+    print(analysis.target)
