6f33c304506d074cf76dc401419cc701e20c508b
isabarrenha <barrenha95@yahoo.com.br>
2024-07-04 08:51:22 -0300

    exploratory-split-df-target

--- scripts/utilities.py
+++ scripts/utilities.py
@@ -3,11 +3,28 @@
 import pandas as pd
 
 class ExploratoryAnalysis:
-    """This class was created to turn exploratory analysis easier"""
+    """
+    This class was created to turn exploratory analysis easier
+    
+    Operations:
+    - Count by category
+    - Events by category (Positive answer in target column)
+    - Non-events by category (Positive answer in target column)
+    - Exposure (events/total)
+    - Exposure ratio (category expose / dataframe exposure)
+    - Category WOE (Weight of Evidence)
+    - Category IV (Information Value)
+    - Total IV (Information Value)
+    - The percentage rep those operations
 
+    """
+
+    # Getting input from user
     def __init__(self, dataframe, target):
-        self.dataframe = dataframe
-        self.target = target
+
+        # Splitting dataframe from target
+        self.dataframe = dataframe.drop(str(target), axis='columns')
+        self.target = dataframe[str(target)]
 
 
 """Debug generating an generic small dataframe to check if the functions works properly"""
@@ -17,3 +34,16 @@ if __name__ == '__main__':
     analysis = ExploratoryAnalysis(dataframe = df, target = 'Diabetes')
     print(analysis.dataframe)
     print(analysis.target)
+
+
+
+# Table
+## Column | Category | Count | Events | Non-events
+
+# Pipeline
+## Create a list with the name of the columns
+## Catch one column name
+## Create a list of each category
+## Make operations in each category
+## Turn into table
+## Bind to the output table
