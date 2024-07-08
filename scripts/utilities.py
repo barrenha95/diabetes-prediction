798b36fa4d906cb572ec593a5e5d453f0f4e3072
isabarrenha <barrenha95@yahoo.com.br>
2024-07-08 17:11:27 -0300

    creation-count-table-logic

--- scripts/utilities.py
+++ scripts/utilities.py
@@ -1,6 +1,7 @@
 # General-purpose utility scripts or helper functions.
 
 import pandas as pd
+from copy import copy
 
 class ExploratoryAnalysis:
     """
@@ -20,11 +21,12 @@ class ExploratoryAnalysis:
     """
 
     # Getting input from user
-    def __init__(self, dataframe, target):
+    def __init__(self, dataframe, target, ignore):
 
-        # Splitting dataframe from target
-        self.dataframe = dataframe.drop(str(target), axis='columns')
-        self.target = dataframe[str(target)]
+        # Splitting dataframe from target and removing columns to ignore
+        self.target = dataframe[str(target)] # Target column
+        self.dataframe = dataframe.drop(ignore, axis='columns') # List of columns to ignore
+        self.dataframe = self.dataframe.drop(str(target), axis='columns') # Dataframe with the remaining columns
 
         # Getting columns name
         self.columns = list(self.dataframe.columns)
@@ -38,31 +40,47 @@ class ExploratoryAnalysis:
         count = values.value_counts()
 
         return(count)
+    
+    def exploratory_table(self):
+        
+        final_table = None #Declaring final table that will contain the resultes of the exploratory analysis
+
+        for i in self.dataframe.columns:
+            temp = pd.DataFrame(analysis.counting_categories(i)) #Counting table for each category
+            temp['Column'] = i #Create a column with the column name that was counted
+            temp['Category'] = temp.index #Create a column with the category name that was counted
+            temp = temp.reset_index() 
+            temp = temp.drop(columns=i) #Remove the old index (it were the category name)
+            temp = temp.iloc[:, [1,2,0]] #Order the columns
+            
+            if final_table is None:
+                final_table = copy(temp)
+
+            if final_table is not None:
+                final_table = pd.concat([final_table, temp], axis = 0)
+
+        return final_table
+
+            
+
 
 
 
 
 """Debug generating an generic small dataframe to check if the functions works properly"""
 if __name__ == '__main__':
-    data = [['tom', 'athletic','f',18, 0],['erika', 'sedentary','f',22, 0],['robert', 'sedentary','m',56, 1]]
+    data = [['tom', 'athletic','f',18, 0]
+           ,['erika', 'sedentary','f',22, 0]
+           ,['robert', 'sedentary','m',56, 1]]
+    
     df = pd.DataFrame(data, columns=['Name', 'Activity', 'Sex', 'Age', 'Diabetes'])
-    analysis = ExploratoryAnalysis(dataframe = df, target = 'Diabetes')
-    print(analysis.dataframe)
-    print(analysis.target)
-    print(analysis.columns)
+    analysis = ExploratoryAnalysis(dataframe = df, target = 'Diabetes', ignore=['Name'])
 
-    for i in analysis.columns:
-        print(analysis.counting_categories(i))
+    final_table = analysis.exploratory_table()
+    print(final_table)
+
 
 
 
 # Table
 ## Column | Category | Count | Events | Non-events
-
-# Pipeline
-## Create a list with the name of the columns
-## Catch one column name
-## Create a list of each category
-## Make operations in each category
-## Turn into table
-## Bind to the output table
