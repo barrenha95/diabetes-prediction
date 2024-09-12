12dcb1f27508d31c64ea8ff2286d7b4d85a17bcf
isabarrenha <barrenha95@yahoo.com.br>
2024-09-12 15:46:20 -0300

    reading-pickles

--- scripts/inference.py
+++ scripts/inference.py
@@ -1,6 +1,6 @@
 import json
 import pandas as pd
-import numpy as np
+import joblib
 
 class InferenceEngine:
     '''
@@ -31,11 +31,13 @@ class InferenceEngine:
         except:
             self.request = json.loads(request)
 
-        #self.df = pd.json_normalize(self.request, max_level=0)
+
+        self.sc = joblib.load('./objects/std_scaler.bin')
+        self.lr = joblib.load('./objects/logistic_regressor.bin')
 
     def bmi_categorization(self, name = "BMI"):
 
-        bmi_tmp = self.request['BMI']
+        bmi_tmp = self.request[name]
 
         binInterval  = [0, 18.4, 24.9, 29.9, 34.9, 39.9, 100]
         binLabels= ['Underweight', 'Normal', 'Overweight', 'Obesity1', 'Obesity2', 'Obesity3']
@@ -43,28 +45,37 @@ class InferenceEngine:
         bmi_tmp = pd.cut(x=[float(bmi_tmp)], bins=binInterval, labels=binLabels) 
 
         tmp = self.request
-        tmp['BMI'] = bmi_tmp
+        tmp[name] = bmi_tmp
 
         self.request_categorized = tmp
         
         return self.request_categorized
+    
+    def standardization(self):
+
+        tmp = pd.DataFrame.from_dict(self.request_categorized)
+
+        #self.X_train_std = self.sc.fit_transform(tmp)
+        #return self.X_train_std
+        
         
         
 
 """Debug generating an generic small dataframe to check if the functions works properly"""
 if __name__ == '__main__':
 
-    rq1 = {"HighBP":"1",
-           "HighChol":"0",
-           "BMI":"30",
-           "HeartDiseaseorAttack":"1",
-           "PhysActivity":"1",
-           "DiffWalk":"1",
-           "Age":"1",
-           "Education":"1",
-           "Income":"1"
+    rq1 = {           
+           "Age":"9",
+           "Income":"3",
+           "PhysHlth":"15",
+           "MentHlth":"18",
+           "Education":"4",
+           "GenHlth":"5",
+           "BMI":"40",
+           "HighBP":"1",
+           "Smoker":"1",
+           "Fruits":"0"
            }
-    
+
     inference = InferenceEngine(request = rq1)
-    
     print(inference.bmi_categorization())
